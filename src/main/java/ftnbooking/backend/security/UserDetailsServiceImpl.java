2804fedb93dbfabcea36266cb87839a07675b6be
BojanStipic <bojanstipic@gmail.com>
2018-05-22 23:15:34 +0200

    Spring Security with JWT authentication

---
+++ src/main/java/ftnbooking/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package ftnbooking.backend.security;
+
+import static java.util.Collections.emptyList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import ftnbooking.backend.users.ApplicationUser;
+import ftnbooking.backend.users.ApplicationUserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private ApplicationUserRepository applicationUserRepository;
+
+	public UserDetailsServiceImpl(ApplicationUserRepository applicationUserRepository) {
+		this.applicationUserRepository = applicationUserRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		ApplicationUser applicationUser = applicationUserRepository.findByEmail(username);
+		if (applicationUser == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new User(applicationUser.getEmail(), applicationUser.getPassword(), emptyList());
+	}
+
+}
