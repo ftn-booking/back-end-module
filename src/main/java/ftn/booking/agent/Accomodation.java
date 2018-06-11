2ee3049673acb5a9f436212a12593fc7dbbf415b
Miloš Ostojić <milos.ostojic747@gmail.com>
2018-06-11 16:08:00 +0200

    ..

--- src/main/java/ftn/booking/agent/Accomodation.java
+++ src/main/java/ftn/booking/agent/Accomodation.java
@@ -3,18 +3,22 @@ package ftn.booking.agent;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name="Accomodation")
+@Entity
 public class Accomodation {
 
 	@XmlElement(required = true)
