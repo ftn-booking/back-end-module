8e413320c101f069f6331560f6e413395146250b
BojanStipic <bojanstipic@gmail.com>
2018-06-19 22:03:22 +0200

    Add comment controller

--- src/main/java/ftnbooking/backend/comments/CommentController.java
+++ src/main/java/ftnbooking/backend/comments/CommentController.java
@@ -1,10 +1,37 @@
 package ftnbooking.backend.comments;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ftnbooking.backend.lodgings.Lodging;
+import ftnbooking.backend.lodgings.LodgingService;
 
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
+
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private LodgingService lodgingService;
+
+	@GetMapping("/lodging/{id:\\d+}")
+	public ResponseEntity<List<Comment>> getComments(@PathVariable Long id) {
+		Lodging lodging = lodgingService.findOne(id);
+		if(lodging == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		List<Comment> comments = commentService.findByLodgingAndApproved(lodging, Approval.APPROVED);
+		if(comments == null || comments.isEmpty())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(comments, HttpStatus.OK);
+	}
 
 }
