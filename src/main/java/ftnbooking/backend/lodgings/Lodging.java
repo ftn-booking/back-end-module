5b404e435d043b28d82cc2901f376d7053b856f1
Kaishaku95 <njaridaniel@gmail.com>
2018-06-22 16:34:59 +0200

    minor adjustment

--- src/main/java/ftnbooking/backend/lodgings/Lodging.java
+++ src/main/java/ftnbooking/backend/lodgings/Lodging.java
@@ -106,7 +106,7 @@ public class Lodging {
 
 */
 	@XmlElement(required = true)
-	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
 	private Set<FeatureType> featureType;
 
 	@XmlElement(required = true)
