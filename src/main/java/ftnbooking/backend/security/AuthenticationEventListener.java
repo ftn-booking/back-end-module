273d7c6ce0ee2c6c98491c0449eb172ced96d371
Miloš Ostojić <milos.ostojic747@gmail.com>
2018-06-27 09:43:35 +0200

    Customer logging.

--- src/main/java/ftnbooking/backend/security/AuthenticationEventListener.java
+++ src/main/java/ftnbooking/backend/security/AuthenticationEventListener.java
@@ -9,10 +9,13 @@ import org.springframework.stereotype.Component;
 
 import ftnbooking.backend.users.ApplicationUser;
 import ftnbooking.backend.users.ApplicationUserService;
+import ftnbooking.logging.LoggerManager;
 
 @Component
 public class AuthenticationEventListener {
 
+	@Autowired
+	private LoggerManager logger;
 	@Autowired
 	private ApplicationUserService applicationUserService;
 
@@ -20,6 +23,7 @@ public class AuthenticationEventListener {
 	public void authenticationFailed(AuthenticationFailureBadCredentialsEvent event) {
 		String email = (String) event.getAuthentication().getPrincipal();
 		ApplicationUser user = applicationUserService.findOne(email);
+		logger.logAdmin("AUTHENTICATION FAILED: ", email);
 		applicationUserService.failedLogin(user);
 	}
 
@@ -28,6 +32,7 @@ public class AuthenticationEventListener {
 		User principal = (User) event.getAuthentication().getPrincipal();
 		String email = principal.getUsername();
 		ApplicationUser user = applicationUserService.findOne(email);
+		logger.logAdmin("AUTHENTICATION SUCCEEDED: ", email);
 		applicationUserService.successfulLogin(user);
 	}
 
